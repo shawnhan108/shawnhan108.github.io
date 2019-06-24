65be6f0b5efab8b1c99e5940ea0c367500a4d767
SHAWN <shawnhan1999@hotmail.com>
2019-06-23 20:01:52 -0400

    debug contact form
    Eliminated non-exixtent title variable in php file

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -1,12 +1,10 @@
 <?php
 	$name = $_POST['name'];
-	$title = $_POST['title'];
 	$visitor_email = $_POST['email'];
 	$message = $_POST['message'];
 	$email_from = 'y262han@edu.uwaterloo.ca';
 	$email_subject = "Yushuo Website Form Submission";
 	$email_body = "User Name: $name.\n".
-					"User Title: $title. \n".
 					"User Email: $visitor_email.\n".
 					"User Message: $message.\n";
 	$to = "y262han@edu.uwaterloo.ca";
